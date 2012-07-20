f638acab98e3946e213957dd2beea767d977a8ea
Jukka Svahn <web@rahinaa.biz>
2012-07-20 21:16:06 +0300

    Changed #warning to a class .alert-block.warning.

--- rah_post_versions.php
+++ rah_post_versions.php
@@ -809,7 +809,7 @@ EOF;
 		echo '<h1 class="txp-heading">'.gTxt('rah_post_versions').'</h1>'.n;
 		
 		if(self::get()->nowrite == true) {
-			echo '<p id="warning">'.gTxt('rah_post_versions_repository_data_missing').'</p>';
+			echo '<p class="alert-block warning">'.gTxt('rah_post_versions_repository_data_missing').'</p>';
 		}
 		
 		echo '<div class="txp-container">'.n.
